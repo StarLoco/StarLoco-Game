bb9277229f4729330a335a4b7eb65a90bcb5e3a4
Diabu <diabu@diabu.com>
2023-04-08 10:53:14 -0400

    Missing newline

--- src/org/starloco/locos/script/proxy/SPlayer.java
+++ src/org/starloco/locos/script/proxy/SPlayer.java
@@ -56,6 +56,7 @@ public class SPlayer extends DefaultUserdata<Player> {
     private static int level(Player p) {
         return p.getLevel();
     }
+
     @SuppressWarnings("unused")
     private static int breed(Player p) {
         return p.getClasse();
