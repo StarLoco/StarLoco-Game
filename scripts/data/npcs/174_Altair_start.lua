63c95d6b3d2ae1e8d3ce4894b14c4073569eed9f
Diabu <diabu@diabu.com>
2023-04-08 21:00:36 -0400

    Fix Altair

--- scripts/data/npcs/174_Altair_start.lua
+++ scripts/data/npcs/174_Altair_start.lua
@@ -3,7 +3,7 @@ local npc = Npc(174, 9059)
 npc.colors = {6380875, 16777215, 11250604}
 
 function npc:onTalk(p, answer)
-    BouftousDungeon:onTalkToGateKeeper(p, answer)
+    GobballDungeon:onTalkToGateKeeper(p, answer)
 end
 
 RegisterNPCDef(npc)
