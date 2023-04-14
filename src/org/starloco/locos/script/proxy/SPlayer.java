cff16e167456ddbb9a59bd1dd1075e0bfeb47787
Diabu <diabu@diabu.com>
2023-04-13 21:28:07 -0400

    Add SPlayer:isDead

--- src/org/starloco/locos/script/proxy/SPlayer.java
+++ src/org/starloco/locos/script/proxy/SPlayer.java
@@ -73,6 +73,11 @@ public class SPlayer extends DefaultUserdata<Player> {
     }
 
     @SuppressWarnings("unused")
+    private static boolean isDead(Player p) {
+        return p.isDead() == 1;
+    }
+
+    @SuppressWarnings("unused")
     private static void sendAction(Player p, ArgumentIterator args) {
         int actionID = args.nextInt();
         String actionIDStr = "";
