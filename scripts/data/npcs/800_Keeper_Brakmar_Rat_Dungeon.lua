a0fc84c4f80bdb1385841b06f750ac519d547b95
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-04-17 16:55:59 +0200

    fix wrong mapid

--- scripts/data/npcs/800_Keeper_Brakmar_Rat_Dungeon.lua
+++ scripts/data/npcs/800_Keeper_Brakmar_Rat_Dungeon.lua
@@ -4,7 +4,7 @@ local npc = Npc(800, 1535)
 function npc:onTalk(p, answer)
     if p:mapID() == 6738 then 
         BrakmarRatDungeon:onTalkToGateKeeper(p, answer)
-    elseif p:mapID() == 10213 then
+    elseif p:mapID() == 10199 then
         if answer == 0 then 
             p:ask(3294, {2963})
         elseif answer == 2963 then
