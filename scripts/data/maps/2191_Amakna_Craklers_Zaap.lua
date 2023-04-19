ec6140e8a850b00059b83c77ee227c8b90120808
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-04-19 07:39:11 +0200

    add goultard npcs + tournament maps

---
+++ scripts/data/maps/2191_Amakna_Craklers_Zaap.lua
@@ -0,0 +1,64 @@
+local map = MapDef(
+        2191,
+        "0706131721",
+        "222E324A4224502C585F606A4452325D704047233B21205C232532624E742F6768542D7B795B5A292C406B2043236752593F305B293C41473439667D396F5F7A7E343663687A444F457E22556B247C2F772F3A6C253235237E4F4A64253262736370613240392F4E58765F7A30777E48536231253262762C28305C496628504748565F473B452F70295739634A76495A7025326246325C26237A3E294A40413D7C31523E2E335F5358506D37486A6A2C3F61634A6F525D31543958425D7E70356A226C716B39205338314C7E45415A69752C3E2F5F587B3C215C634C47396C6D6B44537D5C346C4678293C492532356564244A724F253262",
+        "HhbfeaaaaaHhbfeaaaaaHhbfeaaaaaHhbfeaaaaaHNa5eaaaaaHhaaeaaaaaHhaaemHWaaHhakeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaH3a5eaaaaaHhbfeaaaaaHhbfeaaadyHhbfeaaaaaHhbfeaaadyHhbfeaaadyHhbfeaaadyHNG5eaaaaaHhGaeaaaaaHhGaeaaaaaHhqkeqgaaaHhGaemHqaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaH3G5eaaaaaHhHfeaaaaaHhbfeaaadyHhbfeaaaaaHhbfeaaadPH3G6eaaaaaHNG5eaaaaaHhGaem0aaaHhGaeaaaaaHhGaeb4WaaHhGaeb4qaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaH3a5eaaaiGHhHfeaaaaaHhbfeaaadyHhbfeaaadyHNG6eaaaaaHNG7eaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhWkeaaaaaHhGaemHaaaHhGaemHWaaHhGaeb4aaaHhGaeb4aaaHhGaemHaaaH3a5eaaadyHhbfeaaadPHhbfeaaaaaHhN3eaaaaaHxG7eaaaaaHhGaemHWaaHhGaemHaaaHhGaeaaaaaHhGaemHGaaHhWkeaGaaaHhGaeb4aaaHhWkeaaaaaHhWkeaaaaaHhWkeaGaaaHhGaemHaaaGha6eaaaaaHhbfeaaadyHhHfeaaaaaHNG6eaaaaaHhGaemHWaaHhWkeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaemHGaaHhWkeaaaaaHhWkeaaaaaHhGaeb4GaaHhGaeb4GaaHhWkeaaaaaH3a7eaaaiMGhbfeaaaaaHhbfeaaadPHhN3eaaaaaHxG7eaaaaaHhWkeaGaaaHhGaemHqaaHhGaeaaaaaHhGaeaaaaaHhGaemHGaaHhGaeb4GaaHhGaemHqaaHhGaeaaaaaHhGaeb4WaaHhGaeb4qaaHhG6eaaaaaHhbfeaaaaaHhHfeaaaaaHNG5eaaaaaHhGaemHGaaHhGaemHqaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaemHWaaHhWkeaaaaaH3G7eaaaaaHhbfeaaadyHhbfeaaadyHNG5eaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaem0aaaHhWkeaaaaaHhGaemHqaaH3G5eaaaaaHhbfeaaadyHNG5eaaaaaHhGaem0aaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaemHWaaHhWkeaaaaaHhGaemHqaaHhGaeaaaaaH3G5eaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhWkeaaaaaHhGaeb4qaaHhGaeaaaaaHhGaem0aaaHNa7eaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaemHGaaHhWkeaaaaaGhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaemHGaaHhiaeaadTyHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaGhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhqaeqgaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaabJHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhGaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaabHHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhbfeaaaaaHhaaeaaabAHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaabEHhaaeaaaaaHhaaeaaaaaHhaaeaaabEHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhbfeaaaaaHhaaeaaabAHhaaeaaaaaHhaaeaaaaaHhaaeaaabBHhbfeaaabAHhaaeaaaaaHhaaeaaabBHhaaeaaabAHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhbfeaaaaaHhaaeaaabAHhaaeaaaaaHhaaeaaabBHhh2eaaadyHhaaeaaabAHhaaeaaabBHhh2eaaadyHhaaeaaabAHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaGhbfeaaaaaHhbfeaaaiGHhaaeaaabAHhaaeaaabBHhh2eaaadyHhh2eaaaaaHhaaeaaabCH3h7eaaaaaHhh2eaaadyHhaaeaaabAHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhbfeaaaaaH3a5eaaaaaHhbfeaaaaaHhaaeaaabCGhh2eaaaiSHhh2eaaaaaHhh_eaaaaaHNh9eaaaaaHxh_eaaaaaHhh2eaaaaaHhaaeaaabAHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhbfeaaaaaHhbfeaaaaaH3a5eaaaaaHhbfeaaaaaHxh_eaaaaaH3h7eaaaaaHhh_eaaaaaHhbfeaaaaaHhbfeaaaaaHxh_eaaaaaHhh_eaaaaaHhaaeaaabAHhaaeaaaaaGhaaeaaaaaHhh2eaaadyHhh2eaaaaaHhaaeaaaiMH3a5eaaaaaHhbfeaaaaaHNh9eaaaaaHNh9eaaadPH3a6eaaaaaH3a6eaaaaaHhbfeaaaaaHNh9eaaaaaHhbfeaaaaaHhaaeaaabAHhaaeaaaaaGhh2eaaaiGHhh2eaaaaaHhh2eaaaaaHhaaeaaaaaH3a5eaaaaaH3a6eaaaaaHhbfeaaaaaHNa5eaaaaaHNa7eaaaaaH3a5eaaaaaHhbfeaaaaaH3a6eaaaaaHhbfeaaaaaHhaaeaaabCGhh2eaaabAG3h7eaaaaaHhh2eaaaaaHhaaeaaaaaHhaaeaaaaaHNa7eaaaaaH3a5eaaaaaHNa5eaaaaaHhaaeaaaaaHhaaeaaaaaH3a5eaaaaaHNa5eaaaaaH3a5eaaaaaHhbfeaaaaaHxh_eaaaaaGhh_eaaabAGxh_eaaaaaHhh_eaaaaaHhaaem0aaaHhaaeaaaaaHhaaeaaaaaHNa7eaaaaaHhaaeaaaaaHhaaem0aaaHhaaeaaaaaHNa7eaaaaaHhaaeaaaaaH3a5eaaaaaHhbfeaaaaaHNh9eaaaaaGhbfeaaabAHhh_eaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHNa5eaaaaaHhbfeaaaaaHhbfeaaaaaGhbfeaaabAHhbfeaaaaa",
+        15,
+        17,
+        -5,
+        -8,
+        2
+)
+
+map.positions = "a-babmbpbCbObQb3|bXb-cacmcSc6dgdv"
+map.capabilities = 98
+map.mobGroupsCount = 2
+
+-- '1;1;1;0;0;0;0' forbiddens -> capabilities ? Or script ?
+
+map.allowedMobGrades = {
+    {31, 1},
+    {31, 2},
+    {31, 3},
+    {31, 4},
+    {31, 5},
+    {34, 1},
+    {34, 2},
+    {34, 3},
+    {34, 4},
+    {34, 5},
+    {46, 1},
+    {46, 2},
+    {46, 3},
+    {46, 4},
+    {46, 5},
+    {61, 1},
+    {61, 2},
+    {61, 3},
+    {61, 4},
+    {61, 5},
+    {62, 1},
+    {62, 2},
+    {62, 3},
+    {62, 4},
+    {62, 5},
+    {106, 1},
+    {106, 2},
+    {106, 3},
+    {106, 4},
+    {106, 5},
+    {483, 1},
+    {483, 2},
+    {483, 3},
+    {483, 4},
+    {483, 5}
+}
+
+map.onMovementEnd = {
+    [202] = moveEndTeleport(7832, 233),
+    [21] = moveEndTeleport(2072, 441)
+}
+
+map.zaapCell = 200
+
+RegisterMapDef(map)
