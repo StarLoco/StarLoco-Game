58b534f3e79e0aac2c4d867bec73f2e0839d321d
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-04-22 07:42:37 +0200

    add scaraleaf dungeon

---
+++ scripts/data/dungeons/Scaraleaf.lua
@@ -0,0 +1,32 @@
+local group1 = {
+    {797, {1,2,3,4,5}},
+    {795, {1,2,3,4,5}},
+    {795, {1,2,3,4,5}},
+    {795, {1,2,3,4,5}},
+    {194, {1,2,3,4,5}},
+    {198, {1,2,3,4,5}},
+    {240, {1,2,3,4,5}},
+    {241, {1,2,3,4,5}}
+}
+
+local mapInfos = {
+    [9775] = {winDest= {9776,422}},
+    [9778] = {winDest= {9779,436}},
+    [9779] = {winDest= {9780,436}},
+    [9780] = {winDest= {9781,394}},
+    [9781] = {winDest= {9782,436}},
+    [9782] = {winDest= {9784,436}},
+    [9784] = {groupCell= 178, group= group1, winDest= {9785,436}}
+}
+
+for k,v in pairs(mapInfos) do
+    local map = MAPS[k]
+    if map then
+        map.onFightEnd[PVMFightType] = fightEndTeleportWinnerPlayers(v.winDest[1], v.winDest[2])
+        if v.group then
+            map.staticGroups = {
+                {v.groupCell, v.group}
+            }
+        end
+    end
+end
---
+++ scripts/data/maps/9776_Scaraleaf_2.lua
@@ -0,0 +1,68 @@
+local map = MapDef(
+        9776,
+        "0706131721",
+        "50206E2A6946465049463948326A264A40393F25326276613B7B663A2847564D633F7A403D68565F71294045724F6F34332052414A39703E5D24404A27342850673D7D62594860616D612D72592F557D563B2F37655645424A715D2E6A4B37467A2A274D30387C3C4729357C3F3A5D784A7345263E796F586A4D70226546634F267B44697E2F265B42326B76586A716C72757D504775236C6340454330575369486445235E28456262236E3F40253262756C447A7A5A297C3A687E5B5C6F70586850495152264E225120325B426A4B3E55732532627A",
+        "HhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaZnHhcieaaaZqHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhcieaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUGhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUHhGaeruaaaGhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUHhGaeruaaaHhGaeruaaaGhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUGhaaeruaYiHhGaeryGaaHhGaeruaaaGhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUHhGaeruaZjHhGaerraaaHhGaertqaaHhGaeruaZjHhqaeqgaYWHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUHhGaertaaaHhGaertaaaHhaaerAaZeHhGaertqaaHhGaeruaaaHhGaeaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaZrHhcieaaaaaGhaaeaaeYUGhaaeruaYXHhGaertaZsHhGaeaaaZjHhGaeaaaaaHhGaertqaaHhGaeruaaaGhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaZrHhcieaaaaaGhaaeaaeYUHhGaeruaaaHhGaersaZjHhaaeaaaY-HhGaeaaaaaHhGaerAaaaHhGaertqZjHhGaeruaaaGhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUHhGaeruaaaHhGaerAaaaHhGaeaaaaaHhGaeA4aaaHhGaeaaaaaHhGaeSIaaaHhGaertqaaGhaaeruaY5GhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUGhaaeruaYiHhGaerraaaHhaaeaaaZbHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaertqY2HhGaeruaA6GhaaeaaaYUHhcieaaaaaHhcieaaaaaHhcieaaaaaGhaaeaaeYUHhGaeruaZjHhGaerraZjHhGaeaaaZjHhGaeaaaaaHhGaeaaaZjHhGaeaaaaaHhGaeaaaZjHhGaeaaaaaHhGaertqaaHhGaeruaZjGhcieaaaYUHhcieaaaaaHhcieaaaaaGhaaeaaeYUHhGaeruaaaHhGaerraaaHhaaeaaaY7HhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaaZeHhGaerxGaaHhGaeruaA6HhcieaaaaaHhcieaaaaaGhaaeaaeYUHhGaeruaA5HhGaerraaaHhGaeaaaaaHhGaeaaaZjHhGaeSIaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaerrGA5HhGaeruaaaHhcierraaaHhcieaaaaaGhaaeaaeYUGhaaeruaYXHhGaerraaaGhaaeaaaYZHhGaeaaaaaHhGaeaaaaaGhaaeaaaY1HhGaeaaaaaHhGaeaaaaaHhaaeaaaY-HhGaerrGZjHhGaeruaZjHhcierraaaHhcieaaaaaGhaaeaaeYUHhGaeruaaaHhGaertaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaZjHhGaeaaaZjHhGaeaaaZjHhGaeaaaaaHhGaeaaaaaHhGaertGaaHhGaeruaaaHhcierraaaHhcieaaaaaHhcieaaaaaHhGaeruaA6HhGaertaZjHhaaeaaaZeHhaaeaaaZiHhGaeaaaaaHhGaeaaaZjHhGaeaaaaaHhGaeaaaaaHhaaeaaaZbHhGaertGaaHhGaeruaZkHhcierraaaHhcieaaaaaHhcieaaaaaHhGaeruaaaHhGaertaaaHhGaeaaaaaHhaaeaaaZhHhGaeSIaaaHhGaeaaaZjHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaertGaaHhGaeruaZjHhcierraaaHhcieaaaaaHhcieaaaaaHhciervaaaHhGaeryGaaHhaaeaaaY-HhGaeA4aZjHhGaeSIaaaHhGaeaaaZkHhGaeaaaaaHhGaeaaaaaHhaaeaaaY7HhaaeryWZhHhaaeruaZhHhciersaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhGaertaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaaY3HhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaerrGY5GhaaerueY2HhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcierqGaaHhaaeaaaZbHhGaeaaaaaHhGaeaaaaaHhGaeaaaZjHhGaeaaaA5HhGaeaaaZjGhaaeaaaYZHhGaerrGA6HhGaeruaA6HhciersaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhGaertWaaHhGaeaaaaaHhGaeaaaaaHhGaeSIaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaerBaaaHhGaeruaA5HhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcierrqaaHhGaertWZjHhGaeaaaaaHhGaeaaaaaHhGaeaaaZlHhGaeaaaZjHhaaeaaaZeHhGaerrGaaHhGaeruaZjHhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcierrqaaHhGaertWaaHhGaeaaaZuHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeryaaaHhGaeruaaaHhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcierrqaaHhGaeryqZjHhGaeaaaZjHhGaeaaaZjGhaaeaaaY-HhGaertGZjHhGaeruaZjHhciersaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcierrGaaHhaaertaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaertGaaHhGaeruaaaHhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhaaerqGi1HhGaeaaaaaHhGaeryaZjHhaaeaaaZbHhGaeryaaaHhGaeruaaaHhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhciervaaaHhGaertWaaHhGaertGaaHhGaertWaaHhGaerrGaaGhaaeruaY3HhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhGaeruaaaHhGaerqqaaHhciervWaaHhGaertWZjHhGaeruaA6HhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcierrqaaHhciervWaaHhcierraaaHhcierrqaaHhGaeruaaaHhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhciersqaaHhcierraaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaaHhcieaaaaa",
+        15,
+        17,
+        1,
+        26,
+        298
+)
+
+map.positions = "fbfpfqfEfFfTfUf8f9gl|clczcAcOcPc3c4dgdhdv"
+map.capabilities = 46
+map.mobGroupsSize = 1
+
+map.allowedMobGrades = {
+    {795, 1},
+    {795, 2},
+    {795, 3},
+    {795, 4},
+    {795, 5},
+    {798, 1},
+    {798, 2},
+    {798, 3},
+    {798, 4},
+    {798, 5}
+}
+
+map.onMovementEnd = {
+    [112] = moveEndTeleport(9778,436),
+    [198] = moveEndTeleport(9777,253),
+    [243] = moveEndTeleport(9777,253),
+    [244] = moveEndTeleport(9777,253),
+    [151] = moveEndTeleport(9777,253),
+    [196] = moveEndTeleport(9777,253),
+    [286] = moveEndTeleport(9777,253),
+    [194] = moveEndTeleport(9777,253),
+    [254] = moveEndTeleport(9777,253),
+    [253] = moveEndTeleport(9777,253),
+    [223] = moveEndTeleport(9777,253),
+    [193] = moveEndTeleport(9777,253),
+    [192] = moveEndTeleport(9777,253),
+    [252] = moveEndTeleport(9777,253),
+    [267] = moveEndTeleport(9777,253),
+    [356] = moveEndTeleport(9777,253),
+    [281] = moveEndTeleport(9777,253),
+    [111] = moveEndTeleport(9777,253),
+    [201] = moveEndTeleport(9777,253),
+    [155] = moveEndTeleport(9777,253),
+    [108] = moveEndTeleport(9777,253),
+    [138] = moveEndTeleport(9777,253),
+    [325] = moveEndTeleport(9777,253),
+    [384] = moveEndTeleport(9777,253),
+    [383] = moveEndTeleport(9777,253),
+    [353] = moveEndTeleport(9777,253),
+    [323] = moveEndTeleport(9777,253),
+    [293] = moveEndTeleport(9777,253),
+    [263] = moveEndTeleport(9777,253),
+    [381] = moveEndTeleport(9777,253),
+    [380] = moveEndTeleport(9777,253),
+    [439] = moveEndTeleport(9777,253),
+    [409] = moveEndTeleport(9777,253),
+    [379] = moveEndTeleport(9777,253),
+    [346] = moveEndTeleport(9777,253)
+}
+
+RegisterMapDef(map)
