e9c260adad4032dc17e83aed921d598601cfda15
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-04-23 00:47:20 +0200

    add some maps & saphira npc with dialog

--- scripts/data/npcs/764_Saphira.lua
+++ scripts/data/npcs/764_Saphira.lua
@@ -1,11 +1,11 @@
-local npc = Npc(761, 1357)
+local npc = Npc(764, 1357)
 
-npc.colors = {16479068, 16479068, 16479068}
+npc.colors = {7579108, 7579108, 7579108}
 
 ---@param p Player
 ---@param answer number
 function npc:onTalk(p, answer)
-	p:ask(3145)
+    p:ask(3159)
 end
 
 RegisterNPCDef(npc)
