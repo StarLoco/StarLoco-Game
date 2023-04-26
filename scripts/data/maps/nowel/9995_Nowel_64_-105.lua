66cb83e5392b8d55eb9ffb3bc7e1aadb164a925c
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-04-26 07:53:54 +0200

    start to organize scripts folders & fix game.sql

---
+++ scripts/data/maps/nowel/9995_Nowel_64_-105.lua
@@ -0,0 +1,69 @@
+local map = MapDef(
+        9995,
+        "0612011040",
+        "",
+        "HhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDe0EaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaa0KHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaa0KHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaa0LHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDe0FiaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDe0GiaaHhfDeaaaaaHhfDeaaaaaHhfDeaaa0IHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaa0JHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDe0EaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhaae0vaaaHhaae0uaaaHhaaeaaaaaHhaae0vaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaa0IHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaae0wiaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhaae0vaaaHhaae0uaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhfDeaaaaaHhfDe0FaaaHhfDeaaaaaHhfDeaaaaaHhaae0wiaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGae0iaaaHhGaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDe0xiaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaaaaGhaae0iaaaHhGaeaaaaaHhGaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaaaaGhfxeaaa0nGhaaeaaaaaHhGaeaaaaaHhaaeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaaaaGhaaeaaa1vHhGaeaaaaaGhaaeaaaaaGhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaa02HhGaeaaaaaGhaaeaaaaaGhaaeaaaaaGhaaeaaa1jHhGae0haaaGhaaeaaaaaGhaaeaaa1wHhGaeaaaaaHhaaeaaaaaHhfDe0GaaaHhfDeaaaaaHhaaeaaaaaHhGae0yiaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhfBe0ha0rGhaaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaa1rHhGaeaaaaaHhqaeqgaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaa1iHhGaeaaaaaHhaaeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaa0MGhfzeaaa0pHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhf8eaaa1iHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaa0MHhGaeaaaaaHhGaeaaaaaHhGaeaaa1bHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhfDeaaaaaHhfDeaaa0KHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaa0DHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhaaeaaa0DHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGae0haaaHhGaeaaaaaHhGaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaa1vHhGaeaaaaaHhGaeaaaaaHhaaeaaaaaHhfDeaaa0JHhfDeaaaaaHhaaeaaaaaHhGae0yiaaHhGaeaaaaaHhGaeaaa02HhGaeaaaaaHhGaeaaaaaHhqaeqgaaaHhGaeaaaaaHhGaeaaaaaGhaaeaaa1pGhaaeaaa1jHhGaeaaaaaHhfDeaaaaaHhfDeaaaaaHhfDeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaHhaaeaaaaaGhaaeaaa1tGhaaeaaa1kGhaaeaaa0OHhaaeaaaaa",
+        15,
+        17,
+        -65,
+        -105,
+        320
+)
+
+map.positions = "fxfLf0fMdydNd1dM|eMeyekd8dUelezeN"
+map.capabilities = 98
+
+map.allowedMobGrades = {
+    {839, 1},
+    {839, 2},
+    {839, 3},
+    {839, 4},
+    {839, 5},
+    {864, 1},
+    {864, 2},
+    {864, 3},
+    {864, 4},
+    {864, 5},
+    {891, 1},
+    {891, 2},
+    {891, 3},
+    {891, 4},
+    {891, 5},
+    {864, 1},
+    {864, 2},
+    {864, 3},
+    {864, 4},
+    {864, 5},
+    {882, 1},
+    {882, 2},
+    {882, 3},
+    {882, 4},
+    {882, 5},
+    {906, 1},
+    {906, 2},
+    {906, 3},
+    {906, 4},
+    {906, 5},
+    {845, 1},
+    {845, 2},
+    {845, 3},
+    {845, 4},
+    {845, 5},
+    {869, 1},
+    {869, 2},
+    {869, 3},
+    {869, 4},
+    {869, 5},
+    {888, 1},
+    {888, 2},
+    {888, 3},
+    {888, 4},
+    {888, 5}
+}
+
+map.onMovementEnd = {
+    [458] = moveEndTeleport(9996, 38),
+    [347] = moveEndTeleport(9994, 349)
+}
+
+RegisterMapDef(map)
