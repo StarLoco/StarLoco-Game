dd93a710f0bd8374c550ae6632b91f2dd0964635
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-04-26 11:51:17 +0200

    add nowel dungeon 1

--- scripts/data/npcs/795_Hector_Nowel.lua
+++ scripts/data/npcs/795_Hector_Nowel.lua
@@ -3,6 +3,10 @@ local npc = Npc(795, 30)
 npc.colors = {16711680, 16777215, -1}
 npc.accessories = {0, 8334, 8333, 0, 0}
 
+
+local nowel2KeyID = 9460
+local nowel3KeyID = 9459
+
 ---@param p Player
 ---@param answer number
 function npc:onTalk(p, answer)
@@ -24,6 +28,42 @@ function npc:onTalk(p, answer)
             p:endDialog()
         elseif answer == 2904 then
             p:ask(3262)
+        end
+    elseif p:mapID() == 9946 then
+        local hasNowel2Key = p:getItem(nowel2KeyID)
+        local hasNowel3Key = p:getItem(nowel3KeyID)
+        local showKeyResponse = 4614
+        local showKeyResponse2 = 4615
+
+        if answer == 0 then
+            local responses = {4613}
+            if hasNowel2Key then
+                table.insert(responses, showKeyResponse)
+                if hasNowel3Key then
+                    table.insert(responses, showKeyResponse2)
+                end
+            end
+            table.insert(responses, 4616)
+            p:ask(5494, responses)
+        elseif answer == 4613 then
+            p:teleport(9981, 375)
+            p:endDialog()
+        elseif answer == showKeyResponse then
+            if hasNowel2Key then
+                p:teleport(11943, 375)
+                p:endDialog()
+            else
+                p:endDialog()
+            end
+        elseif answer == showKeyResponse2 then
+            if hasNowel3Key then
+                p:teleport(11953, 375)
+                p:endDialog()
+            else
+                p:endDialog()
+            end
+        elseif answer == 4616 then
+            p:endDialog()
         end
     end
 end
