e01f03995d8b0b699c8ddeba97cfadc095309ff1
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-05-10 09:14:52 +0200

    start to add wabbit dofus dungeon

---
+++ scripts/data/npcs/dungeons/149_Gawdien_Dofus.lua
@@ -0,0 +1,21 @@
+local npc = Npc(149, 1056)
+
+---@param p Player
+---@param answer number
+function npc:onTalk(p, answer)
+    local hasAllItems = p:getItem(969) and p:getItem(970) and p:getItem(971)
+    local consumeAllItems = p:consumeItem(969, 1) and p:consumeItem(970, 1) and p:consumeItem (971, 1)
+    local showDofusResponse = 414
+
+    if answer == 0 then
+        local responses = hasAllItems and {showDofusResponse} or {}
+        p:ask(497, responses)
+    elseif answer == showDofusResponse and hasAllItems then
+        if consumeAllItems then
+            p:addItem(972)
+            p:endDialog()
+        end
+    end
+end
+
+RegisterNPCDef(npc)
