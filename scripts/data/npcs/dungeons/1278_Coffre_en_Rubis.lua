83a22997632b6f6735300afe97c0ad9c4f9fd778
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-05-11 04:34:33 +0200

    move dv maps

--- scripts/data/npcs/dungeons/1278_Coffre_en_Rubis.lua
+++ scripts/data/npcs/dungeons/1278_Coffre_en_Rubis.lua
@@ -6,7 +6,9 @@ function npc:onTalk(p, answer)
     if answer == 0 then
         p:ask(7674, {7695, 7696})
     elseif answer == 7695 then
-
+        p:pauseDialog()
+        p:startScenario(94, "7001010000")
+        p:endDialog()
     elseif answer == 7696 then
         p:teleport(1113, 199)
         p:endDialog()
