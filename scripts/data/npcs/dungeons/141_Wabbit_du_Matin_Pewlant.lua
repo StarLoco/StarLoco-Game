6b79a7b96a8cc2c130e8c2522aded70cbb295070
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-05-14 09:49:28 +0200

    add some npcs

---
+++ scripts/data/npcs/dungeons/141_Wabbit_du_Matin_Pewlant.lua
@@ -0,0 +1,18 @@
+local npc = Npc(141, 9054)
+
+---@param p Player
+---@param answer number
+function npc:onTalk(p, answer)
+    if answer == 0 then
+        if p:getItem(961) then
+            p:ask(467)
+        else
+            p:ask(467, {391})
+        end
+        elseif answer == 391 then
+        p:addItem(961)
+        p:endDialog()
+    end
+end
+
+RegisterNPCDef(npc)
