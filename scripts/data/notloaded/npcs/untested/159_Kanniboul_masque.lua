9cb3f0e4d226c2236efd68a604996f20498dea87
Saokkei <56141571+Saokkei@users.noreply.github.com>
2023-05-15 04:54:56 +0200

    fix npc 155 dialog

--- scripts/data/notloaded/npcs/untested/159_Kanniboul_masque.lua
+++ scripts/data/notloaded/npcs/untested/159_Kanniboul_masque.lua
@@ -3,6 +3,7 @@ local npc = Npc(159, 9063)
 ---@param p Player
 ---@param answer number
 function npc:onTalk(p, answer)
+    --TODO: Porté le masque kanniboule & etre level 40 mini
     if answer == 0 then p:ask(566, {493})
     end
 end
--- scripts/data/npcs/moon/155_Kanniboul_cuisinier.lua
+++ scripts/data/npcs/moon/155_Kanniboul_cuisinier.lua
@@ -4,18 +4,21 @@ local npc = Npc(155, 9063)
 ---@param answer number
 function npc:onTalk(p, answer)
     local hasAllItems = p:getItem(1010, 1) and p:getItem(1011, 1) and p:getItem(1012, 1) and p:getItem(1013, 1)
-    if answer == 0 then p:ask(546, {498})
+    if answer == 0 then
+        p:ask(546, {498})
     elseif answer == 498 then
-        local responses = hasAllItems and {492} or {}
-        p:ask(547, responses)
+        p:ask(547, {492})
     elseif answer == 492 then
         local consumeAll = p:consumeItem(1010, 1) and p:consumeItem(1011, 1) and p:consumeItem(1012, 1) and p:consumeItem(1013, 1)
+
         if hasAllItems then
             if consumeAll then
                 p:ask(584, {497})
             else
                 p:endDialog()
             end
+        else
+            p:ask(585)
         end
     elseif answer == 497 then
         p:teleport(1766, 332)
