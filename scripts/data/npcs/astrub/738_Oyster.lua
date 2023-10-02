c775a78359d2ea68921ca2da422e5f4c8b64bd6f
liberodark <liberodark@gmail.com>
2023-10-02 22:15:34 +0200

    Fix Astrub NPC

--- scripts/data/npcs/astrub/738_Oyster.lua
+++ scripts/data/npcs/astrub/738_Oyster.lua
@@ -2,4 +2,11 @@ local npc = Npc(738, 9000)
 
 npc.colors = {16777215, 15129637, 16777215}
 
+---@param p Player
+---@param answer number
+function npc:onTalk(p, answer)
+    if answer == 0 then p:ask(3043)
+    end
+end
+
 RegisterNPCDef(npc)
