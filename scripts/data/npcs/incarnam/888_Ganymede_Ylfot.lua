0e6661a4c9fd9b1a63e1187522a7127241ac5351
ShowMeDawey <899316+ShowMeDawey@users.noreply.github.com>
2023-10-12 21:34:54 +0200

    Fix answer id & action (#176)

--- scripts/data/npcs/incarnam/888_Ganymede_Ylfot.lua
+++ scripts/data/npcs/incarnam/888_Ganymede_Ylfot.lua
@@ -6,9 +6,9 @@ npc.colors = {-1, -1, 16777215}
 ---@param p Player
 ---@param answer number
 function npc:onTalk(p, answer)
-    if answer == 0 then p:ask(3840, {3373, 338})
-    elseif answer == 338 then
-        p:ask(418)
+    if answer == 0 then p:ask(3840, {3373, 3380})
+    elseif answer == 3380 then
+        p:endDialog()
     elseif answer == 3373 then
         local dst = ASTRUB_STATUES[p:breed()]
 
