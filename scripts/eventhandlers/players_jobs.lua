6063a0d7f8af1197d19e1bd31133123d8a3babe3
Diabu <diabu@diabu.com>
2023-10-12 15:56:50 -0400

    Load untested NPCs

---
+++ scripts/eventhandlers/players_jobs.lua
@@ -0,0 +1,22 @@
+
+---@param p Player
+---@param cellID number
+---@param skillID number
+Handlers.players.onSkillUse = function(p, cellID, skillID)
+    if not SKILLS[skillID] then
+        JLogF("unknown skill {}", skillID)
+        return
+    end
+    return SKILLS[skillID](p, cellID)
+end
+
+---@param p Player
+Handlers.players.onJobSkillRequest = function(p)
+    local jobInfo = {}
+
+    for _, jobID in pairs(p:jobs()) do
+        jobInfo[jobID] = {}
+    end
+
+    return jobInfo
+end
