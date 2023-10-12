54e5db957fe0d1efcb65f4eb39d28c2e68638aa7
ShowMeDawey <899316+ShowMeDawey@users.noreply.github.com>
2023-10-12 22:01:32 +0200

    Fix npc 883 dialog

--- scripts/data/npcs/incarnam/883_Fisherman_from_Incarnam.lua
+++ scripts/data/npcs/incarnam/883_Fisherman_from_Incarnam.lua
@@ -3,7 +3,7 @@ local npc = Npc(883, 9008)
 ---@param p Player
 ---@param answer number
 function npc:onTalk(p, answer)
-    if answer == 0 then p:ask(3745)
+    if answer == 0 then p:ask(3695)
     end
 end
 
