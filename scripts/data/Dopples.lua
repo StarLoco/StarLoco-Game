5d9889447ebf203638f3bed30b60276cb3d167cc
Diabu <diabu@diabu.com>
2023-10-14 18:01:51 -0400

    WIP

--- scripts/data/Dopples.lua
+++ scripts/data/Dopples.lua
@@ -1,20 +1,21 @@
 local classBangQuestID = 470
 local requiredLevel = 9
 local questIntervalMs = 82800000 -- 23 Hours
+local rewardInitID = 7106 -- Used on init after killing objective is finished
 local busyInitID = 1834 -- Used when player cannot start new dopple fight
 
--- K: Grade V: {Kamas, XP}
+-- K: Grade V: {Kamas, XPSingle, XPClassBang}
 local rewardsPerGrade = {
-    {100,  1300},
-    {200,  8000},
-    {300,  18000},
-    {400,  30000},
-    {500,  45000},
-    {600,  60000},
-    {700,  85000},
-    {800,  130000},
-    {900,  170000},
-    {1000, 185000},
+    {100,  1300,   7000},
+    {200,  8000,   40000},
+    {300,  18000,  90000},
+    {400,  30000,  150000},
+    {500,  45000,  240000},
+    {600,  60000,  300000},
+    {700,  85000,  450000},
+    {800,  130000, 700000},
+    {900,  170000, 900000},
+    {1000, 185000, 1000000},
 }
 
 ---@param p Player
@@ -29,10 +30,7 @@ local defeatAllStep = QuestStep(995)
 
 defeatAllStep.rewardFn = function(p)
     local rewards = rewardsPerGrade[gradeForPlayer(p)]
-
-    -- TODO
-    -- p:modKamas(rewards[1])
-    -- p:addXP(rewards[2])
+    p:addXP(rewards[3])
 end
 
 
@@ -56,9 +54,27 @@ local dopples = {
         mob = 2691,
         npc = {id=672, gfx=1260},
         quest = {id=466, step=991, bangObjective=3188, fightObjective=3174, npcObjective=3175 },
-        dialog = {init=2851, info=6757, train=6429, trade=6598, classBang=6778, reward=??},
+        dialog = {init=2851, info=6757, train=6429, trade=6598, classBang=6778},
         doploon = 10309,
         certificate = 10296,
+    },
+    -- Enu
+    {
+        mob = 162,
+        npc = {id=440, gfx=30, colors = {16419588, 16710905, 10929091}},
+        quest = {id=464, step=987, bangObjective=3189, fightObjective=3170, npcObjective=3171 },
+        dialog = {init=1764, info=1417, train=172, trade=6752, classBang=6774},
+        doploon = 10305,
+        certificate = 10292,
+    },
+    --Eca
+    {
+        mob = 165,
+        npc = {id=438, gfx=60, colors = {16777215, 16448765, 12498572}, accessories = {0, 0, 2629, 0, 0}},
+        quest = {id=459, step=982, bangObjective=3190, fightObjective=3160, npcObjective=3161 },
+        dialog = {init=1772, info=1425, train=1515, trade=6750, classBang=6769},
+        doploon = 10303,
+        certificate = 10290,
     },
     -- Iop
     {
@@ -68,6 +84,78 @@ local dopples = {
         dialog = {init=1771, info=1424, train=1559, trade=6754, classBang=6776},
         doploon = 10307,
         certificate = 10294,
+    },
+    -- Osa
+    {
+        mob = 161,
+        npc = {id=436, gfx=20, accessories = {0, 2411, 2411, 0, 0}},
+        quest = {id=461, step=984, bangObjective=3192, fightObjective=3164, npcObjective=3165 },
+        dialog = {init=1766, info=1418, train=1485, trade=6755, classBang=6777},
+        doploon = 10308,
+        certificate = 10295,
+    },
+    -- Eni
+    {
+        mob = 166,
+        npc = {id=435, gfx=70, colors = {0xd0000, 0xbb8162, -1}, accessories = {0, 0, 6449, 0, 0}},
+        quest = {id=462, step=985, bangObjective=3193, fightObjective=3166, npcObjective=3167 },
+        dialog = {init=1768, info=1535, train=1559, trade=6747, classBang=6773},
+        doploon = 10304,
+        certificate = 10291,
+    },
+    --Sacri
+    {
+        mob = 455,
+        npc = {id=443, gfx=110, accessories = {0, 6449, 2074, 0, 0}},
+        quest = {id=463, step=986, bangObjective=455, fightObjective=3168, npcObjective=3169 },
+        dialog = {init=1799, info=1519, train=1520, trade=6758, classBang=6779},
+        doploon = 10310,
+        certificate = 10297,
+    },
+    -- Sadi
+    {
+        mob = 169,
+        npc = {id=442, gfx=100, colors = {328193, 12226652, 12226652}, accessories = {0, 0, 772, 0, 0}},
+        quest = {id=465, step=988, bangObjective=3195, fightObjective=3172, npcObjective=3173 },
+        dialog = {init=1770, info=1422, train=1581, trade=6759, classBang=6780},
+        doploon = 10311,
+        certificate = 10298,
+    },
+    -- Feca
+    {
+        mob = 160,
+        npc = {id=433, gfx=10, accessories = {0, 697, 697, 1711, 0}},
+        quest = {id=469, step=992, bangObjective=3196, fightObjective=3180, npcObjective=3181 },
+        dialog = {init=1769, info=1420, train=216, trade=6753, classBang=6775},
+        doploon = 10306,
+        certificate = 10293,
+    },
+    --Cra
+    {
+        mob = 168,
+        npc = {id=439, gfx=90, colors = {7217955, 2902400, 16777215}, accessories = {0, 0, 779, 0, 0}},
+        quest = {id=458, step=982, bangObjective=3197, fightObjective=3147, npcObjective=3148 },
+        dialog = {init=1767, info=1419, train=1509, trade=6697, classBang=6772},
+        doploon = 10302,
+        certificate = 10289,
+    },
+    --Xelor
+    {
+        mob = 164,
+        npc = {id=437, gfx=50, colors = {1245184, 1703936, 16777215}},
+        quest = {id=468, step=991, bangObjective=3198, fightObjective=3178, npcObjective=3179 },
+        dialog = {init=1758, info=168, train=1415, trade=6761, classBang=6782},
+        doploon = 10313,
+        certificate = 10300,
+    },
+    --Sram
+    {
+        mob = 163,
+        npc = {id=441, gfx=40, colors = {10891569, 0, 16050666}, accessories = {0, 699, 677, 0, 0}},
+        quest = {id=467, step=990, bangObjective=3199, fightObjective=3176, npcObjective=3177 },
+        dialog = {init=1773, info=1426, train=1575, trade=6760, classBang=6781},
+        doploon = 10312,
+        certificate = 10299,
     },
 }
 
@@ -92,6 +180,14 @@ local function makeInfoMenu(info)
     end
 end
 
+local rewardPlayer = function(p, doploon)
+    local rewards = rewardsPerGrade[gradeForPlayer(p)]
+
+    p:addItem(doploon)
+    p:modKamas(rewards[1])
+    p:addXP(rewards[2])
+end
+
 local onTalkDoppleMaster = function(info)
     local infoMenu = makeInfoMenu(info)
     local tradeMenu = makeTradeMenu(info)
@@ -106,6 +202,15 @@ local onTalkDoppleMaster = function(info)
                 p:ask(busyInitID, {info.dialog.trade})
                 return
             end
+
+            -- Player finished killing this dopple
+            if doppleQuest:ongoingFor(p) and doppleQuest:hasCompletedObjective(p, info.quest.fightObjective) then
+                doppleQuest:completeObjective(p, info.quest.npcObjective)
+                rewardPlayer(p, info.doploon)
+                p:ask(rewardInitID)
+                return
+            end
+
 
             local responses = {info.dialog.trade}
             if doppleQuest:availableTo(p) or doppleQuest:ongoingFor(p) then
@@ -166,16 +271,10 @@ local createQuest = function(info)
 
     -- Step reward
     defeatSingleStep.rewardFn = function(p)
-        local rewards = rewardsPerGrade[gradeForPlayer(p)]
-
         -- FIXME Certificates are drops on official servers
         -- Remove old certificate
         p:consumeItem(info.certificate, 1)
         p:addItem(info.certificate) -- TODO: Set date on item ?
-
-        p:addItem(info.doploon)
-        p:modKamas(rewards[1])
-        p:addXP(rewards[2])
         return
     end
 
