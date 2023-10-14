3b25ea9ebe1e9a8d629cde4240d600be8e19f9b5
law379 <146428245+law379@users.noreply.github.com>
2023-10-14 23:06:11 +0200

    Add Piwi Quest

--- scripts/data/npcs/incarnam/869_Piwi_q184.lua
+++ scripts/data/npcs/incarnam/869_Piwi_q184.lua
@@ -1,18 +1,45 @@
 local npc = Npc(869, 1212)
+
+local questID = 184
+
 --TODO: Lié à la quête 184
+npc.colors = {16760576, 13055806, 10627125}
+npc.accessories = {8099, 629, 2387, 0, 7080}
+npc.customArtwork = 9087
 npc.gender = 1
+
+npc.quests = {questID}
 
 ---@param p Player
 ---@param answer number
 function npc:onTalk(p, answer)
-    if answer == 0 then p:ask(3710, {3262, 3260})
-    elseif answer == 3260 then p:ask(3723, {3264, 326})
-    elseif answer == 3264 then p:endDialog()
-    elseif answer == 326 then p:ask(408, {327, 328})
-    elseif answer == 328 then p:ask(409)
-    elseif answer == 3262 then p:ask(3724, {3265, 326})
-    elseif answer == 3265 then p:ask(3710, {3262, 3260})
-    elseif answer == 326 then p:ask(408, {327, 328})
+    local quest = QUESTS[questID]
+
+    if quest:availableTo(p) then
+        if answer == 0 then p:ask(3710, {3262, 3260})
+        elseif answer == 3262 then p:ask(3724, {3269, 3265})
+        elseif answer == 3269 or answer == 3260 then p:ask(3723, {3264, 3263})
+        elseif answer == 3264 then p:endDialog()
+        elseif answer == 3263 then p:ask(3725, {3266})
+        elseif answer == 3266 then
+            quest:startFor(p, self.id)
+            p:endDialog()
+        end
+        return
+    end
+
+    if quest:ongoingFor(p) then
+        if quest:tryCompleteBringItemObjectives(p, self.id) then
+            p:ask(3722)
+            return
+        end
+        p:ask(3712)
+        return
+    end
+
+    if quest:finishedBy(p) then
+        p:ask(3711)
+        return
     end
 end
 
