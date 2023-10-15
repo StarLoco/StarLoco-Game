a6a1f1ce768266674dd463202bf9d8415eb7d615
law379 <146428245+law379@users.noreply.github.com>
2023-10-15 21:59:28 +0200

    Ajout quête instinct primaire (#189)

---
+++ scripts/data/quests/incarnam/174_PrimaryInstincts.lua
@@ -0,0 +1,16 @@
+local npcId = 847
+--local mapId = 10295
+
+local qs333 = QuestStep(333, 3573)
+qs333.objectives = {
+    KillMonsterSingleFightObjective(721, 976, 1),
+    KillMonsterSingleFightObjective(722, 974, 1),
+    KillMonsterSingleFightObjective(723, 971, 1),
+    KillMonsterSingleFightObjective(807, 980, 1)
+}
+qs333.rewardFn = function(p)
+    p:addXP(370)
+    p:addItem(8536)
+end
+
+local q174 = Quest(174, {qs333})
