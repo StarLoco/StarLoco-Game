8afd67dd1d10ad7ddeae2aa0b0724adcd1fb375b
law379 <146428245+law379@users.noreply.github.com>
2023-10-16 01:55:57 +0200

    Up hill and down dale (#190)
    * Work In Progres
    
    * Q_179_UpHillAndDownDale

--- scripts/data/npcs/incarnam/862_Milicien_Kerubim.lua
+++ scripts/data/npcs/incarnam/862_Milicien_Kerubim.lua
@@ -1,4 +1,4 @@
-local npc = Npc(861, 9019)
+local npc = Npc(862, 9019)
 
 local questID = 179
 
@@ -8,14 +8,14 @@ function npc:onTalk(p, answer)
     local quest = QUESTS[questID]
 
     if quest:ongoingFor(p) then
-        if quest:canCompleteObjective(p, 748) then
-            p:ask(3690)
-            quest:completeObjective(p, 748)
+        if quest:canCompleteObjective(p, 749) then
+            p:ask(3691)
+            quest:completeObjective(p, 749)
             return
         end
     end
 
-    if answer == 0 then p:ask(3692)
+    if answer == 0 then p:ask(3693)
     end
 end
 
