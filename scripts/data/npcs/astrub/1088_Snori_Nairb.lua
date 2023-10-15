c96b72be48da5e96074eb4e7e38e1b4e2e05f121
Diabu <>
2023-10-14 22:52:22 -0400

    Fix dopples ClassBang XP reward

--- scripts/data/npcs/astrub/1088_Snori_Nairb.lua
+++ scripts/data/npcs/astrub/1088_Snori_Nairb.lua
@@ -5,7 +5,7 @@ npc.customArtwork = 9019
 ---@param p Player
 ---@param answer number
 function npc:onTalk(p, answer)
-    if answer == 0 then p:ask(6169, {6023, 23016})
+    if answer == 0 then p:ask(6169, {6023})
     elseif answer == 6023 then p:endDialog()
     end
 end
