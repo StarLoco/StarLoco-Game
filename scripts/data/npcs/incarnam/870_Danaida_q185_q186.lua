f9e48fc23c387c33bb8db6de64470443b1f1d2b5
law379 <146428245+law379@users.noreply.github.com>
2023-10-21 19:15:07 +0200

    addQuest_brotherWhatThatThing

--- scripts/data/npcs/incarnam/870_Danaida_q185_q186.lua
+++ scripts/data/npcs/incarnam/870_Danaida_q185_q186.lua
@@ -1,5 +1,7 @@
 local npc = Npc(870, 9044)
+
 local questID = 185
+local talkQuestID = 186
 
 npc.gender = 1
 
@@ -9,6 +11,7 @@ npc.quests = {questID}
 ---@param answer number
 function npc:onTalk(p, answer)
     local quest = QUESTS[questID]
+    local talkQuest = QUESTS[talkQuestID]
 
     if quest:availableTo(p) then
         if answer == 0 then
@@ -23,18 +26,28 @@ function npc:onTalk(p, answer)
     if quest:ongoingFor(p) then
         if quest:tryCompleteBringItemObjectives(p, self.id) then
             p:ask(3719)
-            return
         end
-        -- Ongoing quest dialog
+        
         p:ask(3720)
         return
     end
 
-    if quest:finishedBy(p) then
-        p:ask(3719)
+    if talkQuest:ongoingFor(p) then
+        if talkQuest:canCompleteObjective(p, 759) then
+            p:ask(3730, {3276, 3275})
+            talkQuest:completeObjective(p, 759)
+        elseif answer == 3276 or answer == 3275 then p:endDialog()
+        end
         return
     end
-    p:endDialog()
+
+    if quest:finishedBy(p) and talkQuest:finishedBy(p) then
+        p:ask(3721)
+        else
+        p:ask(3719)         
+        return
+    end
+
 end
 
 RegisterNPCDef(npc)
