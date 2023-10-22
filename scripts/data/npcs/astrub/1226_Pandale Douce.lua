f55b6e652ceeb3a4cb0c5aa5f120f489f17f5d56
Majordom <54216216+Majordom34@users.noreply.github.com>
2023-10-22 16:22:49 +0200

    Update pandale douce (#207)
    * AddnpcDutyFree
    
    * Update Pandale douce pour l'erreur
    
    ---------
    
    Co-authored-by: Majordom <54216216+Bountiie@users.noreply.github.com>
    Co-authored-by: Diabu <183968+Diabu@users.noreply.github.com>

--- scripts/data/npcs/astrub/1226_Pandale Douce.lua
+++ scripts/data/npcs/astrub/1226_Pandale Douce.lua
@@ -6,7 +6,6 @@ function npc:onTalk(p, answer)
     if answer == 0 then p:ask(3341, {1303})
     elseif answer == 1303 then p:endDialog()
         p:teleport(10114, 282)
-        p:modkamas(10)
 
     end
 end
